64c81b261f2e0c71056abff3bbd45a0b3a0623e6
Schidu Vasile <sckeedoo@gmail.com>
2017-05-08 18:21:13 +0300

    Last modifications

--- Lab4/src/main/java/Main.java
+++ Lab4/src/main/java/Main.java
@@ -1,58 +1,58 @@
-/**
- * Created by schiduvasile on 5/8/17.
- */
-
-
 import java.util.Properties;
+
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-import javax.mail.PasswordAuthentication;
-
 
 public class Main {
-
     public static void main(String[] args) {
 
+        String to = "sckeedoo@gmail.com";
+
+        String from = "schidu.vasile@gmail.com";
         final String username = "schidu.vasile@gmail.com";
         final String password = "nu6tyupre4ys";
 
-        Properties properties = new Properties();
-        properties.put("mail.smtp.auth", "true");
-        properties.put("mail.smtp.starttls.enable", "true");
-        properties.put("mail.smtp.host", "smtp.gmail.com"); // smtp server addres
-        properties.put("mail.smtp.port", "587"); // port for the smtp server address
-
-        Session session = Session.getInstance(properties,
-                new javax.mail.Authenticator() {
-                    protected PasswordAuthentication getPasswordAuthenticator() {
-                        return new PasswordAuthentication(username, password);
-                    }
-
-                });
+        String host = "smtp.gmail.com";
 
         try {
 
+            Properties props = new Properties();
+            props.put("mail.smtp.auth", "true");
+            props.put("mail.smtp.starttls.enable", "true");
+            props.put("mail.smtp.host", host);
+            props.put("mail.smtp.user", username);
+            props.put("mail.smtp.password", password);
+            props.put("mail.smtp.port", "587");
+            props.put("mail.smtp.auth", "true");
+            props.put("mail.debug", "true");
+
+            Session session = Session.getInstance(props,
+                    new javax.mail.Authenticator() {
+                        protected PasswordAuthentication getPasswordAuthentication() {
+                            return new PasswordAuthentication(username, password);
+                        }
+                    });
+
+
             Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress("schidu.vasile@gmail.com"));
-            message.setRecipient(Message.RecipientType.TO, InternetAddress.parse("sckeedoo@gmail.com"));
-            message.setSubject("My First Mail");
-            message.setContent("This is a test mail, dont be scared");
+            message.setFrom(new InternetAddress(from));
+            message.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse("sckeedoo@gmail.com"));
+            message.setSubject("Testing Subject");
+            message.setText("Hello, this is sample for to check send " +
+                    "email using JavaMailAPI ");
+
             Transport.send(message);
 
-            System.out.println("Message was sent succesfuly");
+            System.out.println("Sent message successfully....");
 
-        }
-        catch (MessagingException e) {
+        } catch (MessagingException e) {
             throw new RuntimeException(e);
         }
-
-
-
-        System.out.println("Helo world!!");
-
     }
 }
